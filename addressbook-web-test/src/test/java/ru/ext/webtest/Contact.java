c50e80779092fe9a7ad0c03786c0f9decab09bcf
d8c28c67-2cbc-4c93-96a5-d087b4816e98 <vzxcnb@gmail.com>
2020-05-09 15:42:05 +0500

    adressbook tests: new group, new contact

---
+++ addressbook-web-test/src/test/java/ru/ext/webtest/Contact.java
@@ -0,0 +1,43 @@
+package ru.ext.webtest;
+
+public class Contact {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String homePhoneNumber;
+    private final String email1;
+    private final String email2;
+
+    public Contact(String firstname, String lastname, String address, String homePhoneNumber, String email1, String email2) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.homePhoneNumber = homePhoneNumber;
+        this.email1 = email1;
+        this.email2 = email2;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhoneNumber() {
+        return homePhoneNumber;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+}
